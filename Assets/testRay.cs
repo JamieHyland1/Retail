93c0ed9ce52c80cb6fc304198eb4904059539d50
Jamie <Jamiehyland01@gmail.com>
2021-07-09 16:26:47 +0100

    Added rows for food, and raycast to detect walls for grid

---
+++ Assets/testRay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testRay : MonoBehaviour
+{
+    public LayerMask mask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          RaycastHit hit;
+       Debug.DrawRay(this.transform.position,Vector3.down,Color.red);
+                if (Physics.Raycast(this.transform.position, Vector3.down, out hit, 300f, mask)){
+                     Debug.Log(hit.collider.gameObject.name);
+                    Debug.DrawRay(this.transform.position,Vector3.down*5,Color.green);
+                }
+                
+    }
+}
